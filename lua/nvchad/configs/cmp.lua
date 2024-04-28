020c9b39d74cceee50e608fc5ef1b1847dc18869
webx <zh@webx.xin>
2024-04-28 20:04:49 +0800

    cmp keys

--- lua/nvchad/configs/cmp.lua
+++ lua/nvchad/configs/cmp.lua
@@ -69,10 +69,14 @@ local options = {
   formatting = formatting_style,
 
   mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<A-k>"] = cmp.mapping.select_prev_item(),
+    ["<A-j>"] = cmp.mapping.select_next_item(),
+    ["<A-[>"] = cmp.mapping.scroll_docs(-4),
+    ["<A-]>"] = cmp.mapping.scroll_docs(4),
+    --[[ ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4), ]]
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
 
