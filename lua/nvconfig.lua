36ba6d67c95a916c975c99c28bad75713f0031da
webx <zh@webx.xin>
2024-05-10 21:01:44 +0800

    ui

--- lua/nvconfig.lua
+++ lua/nvconfig.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.ui = {
   ------------------------------- base46 -------------------------------------
-  -- hl = highlights，need call：requre("base46").load_all_highlights()
+  -- hl = highlights，need call：require("base46").load_all_highlights()
   hl_add = {},
   hl_override = {
     Visual = {
@@ -24,11 +24,11 @@ M.ui = {
     style = "default", -- default/flat_light/flat_dark/atom/atom_colored
   },
 
-  telescope = { style = "borderless" }, -- borderless / bordered
+  telescope = { style = "bordered" }, -- borderless / bordered
 
   ------------------------------- nvchad_ui modules -----------------------------
   statusline = {
-    theme = "default", -- default/vscode/vscode_colored/minimal
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
     separator_style = "default",
@@ -40,7 +40,7 @@ M.ui = {
   tabufline = {
     enabled = true,
     lazyload = true,
-    order = { "treeOffset", "buffers", "tabs", "btns" },
+    order = { "treeOffset", "buffers", "tabs", --[[ "btns"  ]]},
     modules = nil,
   },
 
@@ -61,7 +61,7 @@ M.ui = {
 
     buttons = {
       { "  Find File", "<A-p>", "Telescope find_files" },
-      { "󰈚  Recent Files", "<leader>fo", "Telescope oldfiles" },
+      { "󰈚  Recent Files", "<Space>f", "Telescope oldfiles" },
       { "󰈭  Find Word", "<leader>fw", "Telescope live_grep" },
       { "  Themes", "<leader>th", "Telescope themes" },
     },
